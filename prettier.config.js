13fd0e6321201f610b628f4b0c105cd75aa1c7b1
mzvast <mzvast@gmail.com>
2022-11-08 21:10:41 +0800

    feat: init

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    singleQuote: true,
+    jsxSingleQuote: false,
+    trailingComma: 'all',
+    arrowParens: 'avoid',
+    // trailingComma: 'all',
+    bracketSpacing: false,
+    // jsxBracketSameLine: false,
+    tabWidth: 4,
+    semi: true,
+};
